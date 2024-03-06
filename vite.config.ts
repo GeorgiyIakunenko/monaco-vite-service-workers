28e22d647af95cbb9bc02a21acfbeda7d83aa0d2
Georgiy Iakunenko <goshan3097@gmail.com>
2024-03-06 21:53:00 +0100

    dependency's cleaned up

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {comlink} from "vite-plugin-comlink";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), comlink()],
-  worker : {
-    plugins: [comlink()],
-  }
-})
+  plugins: [react()],
+});
